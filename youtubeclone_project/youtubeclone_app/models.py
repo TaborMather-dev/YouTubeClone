fafcb2a0c467b04c5df7d01bcc04ff810d60917b
Tabor <86973131+TaborMather-dev@users.noreply.github.com>
2021-08-18 10:58:33 -0700

    forgot a few files for the init commit

--- youtubeclone_project/youtubeclone_app/models.py
+++ youtubeclone_project/youtubeclone_app/models.py
@@ -6,3 +6,4 @@ from django.db import models
 class Comments(models.Model):
     userComment = models.CharField(max_length=300)
     like = models.IntegerField()
+    dislikes = models.IntegerField()
