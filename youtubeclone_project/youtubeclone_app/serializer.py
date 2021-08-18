4dd5ec5cb20acc049bc3874e23f1bdf1db7f9a41
Tabor <86973131+TaborMather-dev@users.noreply.github.com>
2021-08-18 16:53:24 -0700

    fixed small errors. API fuctional

--- youtubeclone_project/youtubeclone_app/serializer.py
+++ youtubeclone_project/youtubeclone_app/serializer.py
@@ -5,4 +5,4 @@ from .models import Comment
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ['id', 'userComment', 'like', 'dislike']
+        fields = ['id', 'userComment', 'like', 'dislikes']
